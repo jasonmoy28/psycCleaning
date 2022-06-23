962c2da308bf5ff4bc69c05ab371ef8bea5a0868
Jason Moy <jasonmoy28@gmail.com>
2022-06-23 19:30:02 +0100

    type: <fix> action: <optimize composite_score for faster speed>

--- R/composite_score.R
+++ R/composite_score.R
@@ -3,24 +3,30 @@
 #' The function will perform a rowise aggregation which then divided by the total number of columns. It ignores rows with any NA value (i.e, will use listwise deletion)
 #' @param data dataframe
 #' @param cols vector or tidyselect syntax or helpers. column(s) that need to be composite
-#' @param composite_col_name character. default as 'composited_column'. the column name of the composite column
+#' @param composite_col_name character. default as 'composite_column'. the column name of the composite column
 #'
 #' @return
 #' return a dataframe with a new column with the composite score
 #' @export
 #'
-#' @examples 
+#' @examples
 #' test_df = data.frame(col1 = c(1,2,3,4),col2 = c(1,2,3,4), col3 = c(1,2,NA,4))
-#' composited_df = composite_score(data = test_df)
-#' 
-#' 
-composite_score = function(data, cols = tidyselect::everything(), composite_col_name = 'composited_column'){
-
-return_df = data %>% 
-  dplyr::rowwise() %>% 
-  dplyr::mutate(composite_column = sum(dplyr::c_across(cols =  !!enquo(cols)))) %>% 
-  dplyr::rename(!!enquo(composite_col_name) := 'composite_column') %>% 
-  dplyr::ungroup()
-
+#' composite_df = composite_score(data = test_df)
+#'
+#'
+composite_score = function(data,
+                           cols = tidyselect::everything(),
+                           composite_col_name = 'composited_column') {
+  
+  composite_column = data %>%
+    dplyr::select(!!enquo(cols)) %>%
+    rowSums() %>%
+    tibble::tibble(composite_column = .)
+  
+  return_df = data %>%
+    dplyr::bind_cols(composite_column) %>%
+    dplyr::rename(!!enquo(composite_col_name) := 'composite_column') %>%
+    dplyr::ungroup()
+  
   return(return_df)
 }
