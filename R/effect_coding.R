e32bcde2de9e4981b996a473c9cae83136aba755
Jason Moy <jasonmoy28@gmail.com>
2023-02-28 00:18:10 -0800

    type <major feat> message <added effect coding>

---
+++ R/effect_coding.R
@@ -0,0 +1,20 @@
+#' Effect Coding
+#'
+#' @param data data.frame object 
+#' @param cols vector or tidyselect syntax or helpers. column(s) that need to be re-coded
+#'
+#' @return
+#' return a data.frame with factors being effect coded
+#' @export
+#'
+#' @examples
+#' effect_coding(iris,Species)
+effect_coding = function(data,cols){
+  names = data %>% dplyr::select(!!enquo(cols)) %>% colnames(.)
+  return_df = data %>% dplyr::mutate(dplyr::across(dplyr::all_of(names),~as.factor(.)))
+  for (name in names) {
+    stats::contrasts(return_df[[name]]) = stats::contr.sum(length(unique(return_df[[name]])))
+  }
+  return(return_df)
+}
+
