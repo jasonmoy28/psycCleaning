c3f0d14b6dbefa8227eb3695c10b6aed5b23d5f5
Jason Moy <jasonmoy28@gmail.com>
2023-02-28 00:04:20 -0800

    type <minor feat> message <summarize missing values support (non-)missing count and (non-)missing percentage>

--- R/summarize_missing_values.R
+++ R/summarize_missing_values.R
@@ -4,11 +4,12 @@
 #'
 #' @param data data frame.
 #' @param cols vector or tidyselect syntax or helpers. default is all columns
+#' @param verbose default is `TRUE`. Print the missing value data frame
+#' @param return_result default is `FALSE`. Return `data_frame` if set to yes
 #' @param group character. count missing values by group. 
-#' @param print printing the returned data frame.
 #'
 #' @return
-#' return a data frame with the number of NA values of each columns 
+#' return a data frame with the number of NA values of each columns if `return_result = TRUE`
 #' @export
 #'
 #' @examples
@@ -18,18 +19,44 @@
 summarize_missing_values = function(data,
                                     cols = dplyr::everything(),
                                     group = NULL,
-                                    print = FALSE) {
+                                    verbose = TRUE,
+                                    return_result = FALSE) {
   cols = dplyr::enquo(cols)
   group = dplyr::enquo(group)
-  return_df = data %>%
-    dplyr::group_by(!!group) %>%
-    dplyr::summarize(dplyr::across(!!cols, ~ sum(is.na(.))))
+ 
+  missing_count_df = data %>%
+    dplyr::group_by(!!group) %>% 
+    dplyr::summarize(dplyr::across(!!cols, ~ sum(is.na(.)))) %>% 
+    tidyr::pivot_longer(c(!!cols,-!!group),names_to = 'var_name',values_to = 'miss_count')
+
+  non_missing_count_df = data %>%
+    dplyr::group_by(!!group) %>% 
+    dplyr::summarize(dplyr::across(!!cols, ~ sum(!is.na(.)))) %>% 
+    tidyr::pivot_longer(c(!!cols,-!!group),names_to = 'var_name',values_to = 'non_miss_count')
   
-  if (nrow(return_df) == 1) {
-    return_df = return_df %>% tidyr::pivot_longer(dplyr::everything())
+  total_value_df = data %>%
+    dplyr::group_by(!!group) %>% 
+    dplyr::summarize(dplyr::across(!!cols, ~ dplyr::n())) %>% 
+    tidyr::pivot_longer(c(!!cols,-!!group),names_to = 'var_name',values_to = 'total_value')
+  
+  group_name = data %>% dplyr::select(!!group) %>% colnames()
+  if (length(group_name) == 0) {
+    return_df = total_value_df %>% 
+      dplyr::full_join(missing_count_df,by = 'var_name') %>% 
+      dplyr::full_join(non_missing_count_df,by = 'var_name') %>% 
+      dplyr::mutate(miss_perc = paste0(round(.data$miss_count/.data$total_value*100,3),'%')) %>% 
+      dplyr::mutate(non_miss_perc = paste0(round(.data$non_miss_count/.data$total_value*100,3),'%')) %>% 
+      dplyr::select(c('var_name','miss_count','non_miss_count','miss_perc','non_miss_perc'))
+  
+    } else{
+    return_df = total_value_df %>% 
+      dplyr::full_join(missing_count_df,by = c('var_name',group_name)) %>% 
+      dplyr::full_join(non_missing_count_df,by = c('var_name',group_name)) %>% 
+      dplyr::mutate(miss_perc = paste0(round(.data$miss_count/.data$total_value*100,3),'%')) %>% 
+      dplyr::mutate(non_miss_perc = paste0(round(.data$non_miss_count/.data$total_value*100,3),'%')) %>% 
+      dplyr::select(c('var_name',!!group,'miss_count','non_miss_count','miss_perc','non_miss_perc'))
   }
+  if (verbose) {print(return_df)} 
   
-  if (print) {print(return_df)} 
-  
-  return(return_df)
+  if(return_result){return(return_df)}
 }
