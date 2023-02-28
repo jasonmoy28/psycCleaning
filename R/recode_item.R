0f9c6c66ad262370338fc4166032f555ba3f43d2
Jason Moy <jasonmoy28@gmail.com>
2023-02-28 00:03:31 -0800

    type <fix> message <updated retain_code use along side with code_from and code_to and remove reverse code due to unreliability>

--- R/recode_item.R
+++ R/recode_item.R
@@ -5,50 +5,41 @@
 #' @param code_from vector. the order must match with vector for `code_to`
 #' @param code_to vector. the order must match with vector for `code_from`
 #' @param retain_code vector. Specify the values to be retain 
-#' @param reverse_code logical. Default as `FALSE`. If `TRUE`, it will reverse code the value of that column. 
 #'
 #' @return 
 #' return a data frame  with values that has been recoded
 #' 
 #' @export
 #' @examples
-#' pre_recoded_df = tibble::tibble(x1 = 1:5, x2 = 5:1, x3 = 6:10, y = 1:5)
-#' recoded_df = recode_item(pre_recoded_df, cols = dplyr::contains('x'), reverse_code = TRUE)
-#' list(pre_recoded_df = pre_recoded_df,recoded_df = recoded_df)
+#' pre_recoded_df = tibble::tibble(x1 = 1:5, x2 = 5:1)
+#' recoded_df = recode_item(pre_recoded_df, cols = dplyr::contains('x'),
+#'                         code_from = 1:5,
+#'                         code_to = 5:1)
 #' 
 recode_item <- function(data,
                         cols,
                         code_from = NULL,
                         code_to = NULL,
-                        reverse_code = FALSE,
                         retain_code = NULL) {
   
-  if (is.null(code_from) & reverse_code == FALSE & is.null(retain_code)) {
+  if (is.null(code_from) & is.null(retain_code)) {
     stop()
   }
   
   cols = enquo(cols)
   data = data %>% dplyr::mutate(dplyr::across(!!cols,as.numeric))
-
-  if (reverse_code == TRUE) {
-    # reverse code
-    if (is.null(retain_code)) {
-      # if retain all code
-      return_df = data %>%
-        dplyr::mutate(dplyr::across(!!cols, ~ psych::reverse.code(-1,.)))
-
-    } else{
-      # if retain only selected code
-      return_df = data %>%
-        dplyr::mutate(dplyr::across(!!cols, ~ dplyr::if_else(. %in% retain_code, ., NA_real_))) %>%
-        dplyr::mutate(dplyr::across(!!cols, ~ psych::reverse.code(-1,.)))
-    }
-
-  } else if (!is.null(code_from)){
+  
+  if (!is.null(code_from)){
     # use code from and code to
     names(code_to) = code_from
-    return_df = data %>% 
-      dplyr::mutate(dplyr::across(!!cols, function(lamda) {dplyr::recode(lamda,!!!code_to)}))
+    if (!is.null(retain_code)) {
+      return_df = data %>%
+        dplyr::mutate(dplyr::across(!!cols, ~ dplyr::if_else(. %in% retain_code, ., NA_real_))) %>% 
+        dplyr::mutate(dplyr::across(!!cols, function(lamda) {dplyr::recode(lamda,!!!code_to)}))
+    } else{
+      return_df = data %>% 
+        dplyr::mutate(dplyr::across(!!cols, function(lamda) {dplyr::recode(lamda,!!!code_to)}))
+    }
   } else if (!is.null(retain_code)){
     # only code to NA 
     return_df = data %>%
