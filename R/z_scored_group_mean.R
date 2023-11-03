b46e61870eda5723082e203e44424ba979a3209a
Jason Moy <jasonmoy28@gmail.com>
2023-11-03 08:48:40 -0700

    type: <feat> message: <added back z_scored_group_mean>

--- R/z_scored_group_mean.R
+++ R/z_scored_group_mean.R
@@ -1,15 +1,18 @@
 #' Z scored with with respect to the group mean
-#' Z-scored that uses the group mean in the z-score formula.
+#' 
+#' This function will compute group-mean-centered scores, and then z-scored the group-mean-centered scores with respect to the grand mean.
 #' 
 #' @param data A data.frame or a data.frame extension (e.g. a tibble).
 #' @param cols Columns that need to be centered. See `dplyr::dplyr_tidy_select` for available options. 
 #' @param group the grouping variable. If you need to pass multiple group variables, try to use quos(). Passing multiple group variables is not tested.
 #' @param keep_original default is `FALSE`. Set to `TRUE` to keep original columns
 #' 
+#' @export
 #' @return
-#' return a dataframe with the columns z-scored (replace existing columns)
+#' return a dataframe with a group-mean centered columns that are z-scored with respect to the grand mean. 
 #'
-#'
+#' @examples 
+#' z_scored_group_mean(iris, dplyr::ends_with("Petal.Width"), "Species")
 z_scored_group_mean = function(data,cols,group,keep_original=TRUE) {
   cols = enquo(cols)
   group = enquo(group)
@@ -17,6 +20,8 @@ z_scored_group_mean = function(data,cols,group,keep_original=TRUE) {
   
   return_df = data %>%
     dplyr::group_by(dplyr::across(!!group)) %>%
+    dplyr::mutate(dplyr::across(!!cols, function(x) { (x - mean(x,na.rm = TRUE))})) %>% 
+    dplyr::ungroup() %>% 
     dplyr::mutate(dplyr::across(!!cols, function(x) { (x - mean(x,na.rm = TRUE))/stats::sd(x,na.rm = TRUE)})) %>% 
     dplyr::rename_with(~ paste(.,'_group_z',sep = ''),!!cols) %>% 
     dplyr::ungroup()
---
+++ tests/testthat/test-centering.R
@@ -0,0 +1,32 @@
+testthat::test_that('center_grand_mean', code = {
+  test_df = tibble::tibble(x = 1:5, y = 10:14)
+  centered_df = center_grand_mean(test_df,cols = everything())
+  for (i in 1:length(centered_df$x)) {
+    expect_equal(centered_df$x_grand_c[i], test_df$x[i] - mean(test_df$x))
+  }
+
+  for (i in 1:length(centered_df$y)) {
+    expect_equal(centered_df$y_grand_c[i], test_df$y[i] - mean(test_df$y))
+  }
+})
+
+testthat::test_that('z_scored_grand_mean', code = {
+  centered_col = z_scored_grand_mean(iris,cols = 'Sepal.Length') %>% dplyr::pull('Sepal.Length_grand_z')
+  check_centered_col = scale(iris$Sepal.Length, center = TRUE,scale = TRUE) %>% as.numeric()
+  testthat::expect_equal(centered_col,check_centered_col)
+})
+
+
+testthat::test_that('center_group_mean', code = {
+  centered_col = center_group_mean(iris,cols = 'Sepal.Length',group = 'Species') %>% dplyr::pull('Sepal.Length_group_c')
+  check_centered_col = misty::center(iris$Sepal.Length,type = 'CWC',cluster = iris$Species)
+  testthat::expect_equal(centered_col,check_centered_col)
+})
+
+
+testthat::test_that('z_score_group_mean', code = {
+  centered_col = z_scored_group_mean(iris,cols = 'Sepal.Length',group = 'Species') %>% dplyr::pull('Sepal.Length_group_z')
+  check_centered_col = misty::center(iris$Sepal.Length,type = 'CWC',cluster = iris$Species) %>% scale(center = TRUE,scale = TRUE) %>% as.numeric()
+  testthat::expect_equal(centered_col,check_centered_col)
+})
+
