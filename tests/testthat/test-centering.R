e1f43f02a4685891a9c5633dee8fbd2e642dd716
Jason Moy <jasonmoy28@gmail.com>
2023-11-16 22:58:29 -0800

    type: <misc> message: <minor changes>

--- tests/testthat/test-centering.R
+++ tests/testthat/test-centering.R
@@ -2,16 +2,16 @@ testthat::test_that('center_grand_mean', code = {
   test_df = tibble::tibble(x = 1:5, y = 10:14)
   centered_df = center_grand_mean(test_df,cols = everything())
   for (i in 1:length(centered_df$x)) {
-    expect_equal(centered_df$x_grand_c[i], test_df$x[i] - mean(test_df$x))
+    expect_equal(centered_df$x_c[i], test_df$x[i] - mean(test_df$x))
   }
 
   for (i in 1:length(centered_df$y)) {
-    expect_equal(centered_df$y_grand_c[i], test_df$y[i] - mean(test_df$y))
+    expect_equal(centered_df$y_c[i], test_df$y[i] - mean(test_df$y))
   }
 })
 
 testthat::test_that('z_scored_grand_mean', code = {
-  centered_col = z_scored_grand_mean(iris,cols = 'Sepal.Length') %>% dplyr::pull('Sepal.Length_grand_z')
+  centered_col = z_scored_grand_mean(iris,cols = 'Sepal.Length') %>% dplyr::pull('Sepal.Length_z')
   check_centered_col = scale(iris$Sepal.Length, center = TRUE,scale = TRUE) %>% as.numeric()
   testthat::expect_equal(centered_col,check_centered_col)
 })
